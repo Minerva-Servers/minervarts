51a8ff31857439e76d9415fd0f4f0041be7fc76e
Reece™ <xconnorx@gmx.de>
2023-04-05 01:41:34 +0200

    improve movement

--- gamemode/core/hooks/sh_hooks.lua
+++ gamemode/core/hooks/sh_hooks.lua
@@ -1,33 +1,63 @@
-function GM:Move(ply, mv)
-	local speed = 0.00005
-	local ang = mv:GetMoveAngles()
-	local pos = mv:GetOrigin()
-	local vel = mv:GetVelocity()
-
-	vel = vel + ang:Forward() * mv:GetForwardSpeed() * speed
-	vel = vel + ang:Right() * mv:GetSideSpeed() * speed
-
-	if ( math.abs( mv:GetForwardSpeed() ) + math.abs( mv:GetSideSpeed() ) + math.abs( mv:GetUpSpeed() ) < 0.1 ) then
-		vel = vel * 0.90
-	else
-		vel = vel * 0.99
-	end
-
-	pos = pos + vel
-
-    local trace = util.TraceLine({
-        start = ply:GetPos(),
-        mask = MASK_SOLID_BRUSHONLY,
-        endpos = Vector(0, 0, -1000000),
-    })
-    pos.z = Lerp(0.06, pos.z, trace.HitPos.z + 500)
-
-    if ( pos.z < -1000 ) then
-        pos = Vector(0, 0, 0)
+local zoomDistance = 100
+local zoomSpeed = 25
+function GM:PlayerButtonDown(ply, key)
+    if ( key == MOUSE_WHEEL_DOWN ) then
+        zoomDistance = zoomDistance - zoomSpeed
+    elseif ( key == MOUSE_WHEEL_UP ) then
+        zoomDistance = zoomDistance + zoomSpeed
     end
 
-	mv:SetVelocity(vel)
-	mv:SetOrigin(pos)
+    if ( zoomDistance < 0 ) then
+        zoomDistance = 0
+    elseif ( zoomDistance > 100 ) then
+        zoomDistance = 100
+    end
+end
 
-	return true
+function GM:Move(ply, mv)
+    local speed = 0.00005
+    local ang = mv:GetMoveAngles()
+    local pos = mv:GetOrigin()
+    local vel = mv:GetVelocity()
+
+    vel = vel + ang:Forward() * mv:GetForwardSpeed() * speed
+    vel = vel + ang:Right() * mv:GetSideSpeed() * speed
+
+    if ( math.abs( mv:GetForwardSpeed() ) + math.abs( mv:GetSideSpeed() ) + math.abs( mv:GetUpSpeed() ) < 0.1 ) then
+        vel = vel * 0.90
+    else
+        vel = vel * 0.99
+    end
+
+    // Perform a trace to check if there's an obstacle between the current position and the desired position
+    local trace = util.TraceLine({
+        start = pos,
+        endpos = pos + vel,
+        mask = MASK_PLAYERSOLID_BRUSHONLY,
+        filter = ply
+    })
+
+    // If there's an obstacle, adjust the desired position accordingly
+    if trace.Hit then
+        pos = trace.HitPos - vel:GetNormalized() * 5
+    else
+        pos = pos + vel
+    end
+
+    local trace2 = util.TraceLine({
+        start = pos,
+        endpos = pos + Vector(0, 0, -1000000),
+        mask = MASK_PLAYERSOLID_BRUSHONLY,
+    })
+
+    pos.z = Lerp(0.06, pos.z, trace2.HitPos.z + 500 - zoomDistance * 5)
+
+    if ( pos.z < -1000 ) then
+        pos = Vector(0, 0, 100)
+    end
+
+    mv:SetVelocity(vel)
+    mv:SetOrigin(pos)
+
+    return true
 end
