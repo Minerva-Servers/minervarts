4459ce037032160a34edcb3af786906ab84c4676
Reece™ <xconnorx@gmx.de>
2023-04-05 03:57:41 +0200

    default faction rebels

---
+++ gamemode/factions/sh_rebels.lua
@@ -0,0 +1,18 @@
+local FACTION = {}
+
+FACTION.uniqueID = "faction_rebels"
+FACTION.name = "Rebels"
+FACTION.description = ""
+FACTION.color = Color(229, 153, 25)
+
+FACTION.hudName = "hud_rebels"
+FACTION.startBuilding = "building_reb_headquarters"
+FACTION.startUnit = "unit_rebel"
+
+FACTION.resourceTypes = {
+    ["resource_requisition"] = true,
+    ["resource_scrap"] = true,
+    ["resource_power"] = true,
+}
+
+FACTION_REBELS = minerva.factions.Register(FACTION)
