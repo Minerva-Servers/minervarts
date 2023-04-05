b66dd128a4915c2f5578079a1779373f05455f36
Reece™ <xconnorx@gmx.de>
2023-04-05 03:50:41 +0200

    simple spawn mechanic

--- gamemode/core/hooks/sv_hooks.lua
+++ gamemode/core/hooks/sv_hooks.lua
@@ -1,2 +1,27 @@
+function GM:PlayerInitialSpawn(ply)
+    ply:KillSilent()
 
+    timer.Simple(10, function()
+        ply:Respawn()
+    end)
+end
 
+function GM:PlayerSpawn(ply)
+    local spawnPos = table.Random(ents.FindByClass("info_player_start")):GetPos()
+    ply:SetPos(spawnPos)
+
+    local trace = util.TraceLine({
+        start = ply:GetPos(),
+        endpos = ply:GetPos() - Vector(0, 0, 1000),
+        mask = MASK_PLAYERSOLID,
+        filter = ply
+    })
+
+    // Spawn the entity below the player at the hit position
+    minerva.buildings.Create(BUILDING_REBEL_HQ, function(ent)
+        ent:SetPos(trace.HitPos)
+        ent:DropToFloor()
+
+        ply:SetPos(ent:GetPos() + Vector(0, 0, 2000))
+    end)
+end
