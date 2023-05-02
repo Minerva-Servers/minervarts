ed0438a281445f987599faf174c09796618e2189
eon <dobrin_k1@abv.bg>
2023-05-02 03:52:59 +0300

    Fix not file lua refreshing after first init

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -14,4 +14,8 @@ function GM:Initialize()
 end
 
 function GM:OnReloaded()
+  minerva.util.IncludeDirectory("core/derma")
+  minerva.util.IncludeDirectory("core/libs")
+  minerva.util.IncludeDirectory("core/meta")
+  minerva.util.IncludeDirectory("core/hooks")
 end
