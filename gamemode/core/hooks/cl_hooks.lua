85b6a8f67f17888009727fdbe669f5de48d6f4df
Riggs <xconnorx@gmx.de>
2024-04-18 20:48:54 +0200

    remove, too heavy on performance

--- gamemode/core/hooks/cl_hooks.lua
+++ gamemode/core/hooks/cl_hooks.lua
@@ -3,16 +3,4 @@ function GM:HUDPaint()
     if not ( IsValid(ply) ) then
         return
     end
-    
-    for k, v in ents.Iterator() do
-        if not ( IsValid(v) ) then
-            continue
-        end
-
-        if not ( v:IsNPC() ) then
-            continue
-        end
-
-        minerva.outline.Add(v, color_white, 0)
-    end
 end
